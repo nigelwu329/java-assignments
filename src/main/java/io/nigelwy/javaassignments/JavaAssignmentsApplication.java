b7c09676389baf7cc54ff65f5c9e956001663e4e
Nigel Wu <423595780@qq.com>
2022-04-23 09:53:01 +0800

    feat(N/A): initial commit
    Signed-off-by: Nigel Wu <423595780@qq.com>

---
+++ src/main/java/io/nigelwy/javaassignments/JavaAssignmentsApplication.java
@@ -0,0 +1,13 @@
+package io.nigelwy.javaassignments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAssignmentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAssignmentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/nigelwy/javaassignments/JavaAssignmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nigelwy.javaassignments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAssignmentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
